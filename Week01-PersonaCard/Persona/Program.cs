61dffb7c543bd742da1e4649ee2813bdc875e648
Caozhi Li <git@licaoz.com>
2026-09-05 15:22:07 +0800

    fix: 高 DPI 模式适配

--- Week01-PersonaCard/Persona/Program.cs
+++ Week01-PersonaCard/Persona/Program.cs
@@ -14,8 +14,7 @@ namespace Persona
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
     }
